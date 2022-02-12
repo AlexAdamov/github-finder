ee08d59a3816d3ff383feb0fa5b9029449bd324b
AlexAdamov <80843820+AlexAdamov@users.noreply.github.com>
2022-02-12 16:43:05 +0100

    Added Pages and Routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/layout/navbar";
+import Footer from "./components/layout/footer";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
-    <div>
-    <h1>Hello Internet!!</h1>
+    <Router>
+    <div className="flex flex-col justify-between h-screen">
+      <Navbar />
+      <main className='container mx-auto px-3 pb-12'>
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/about' element={<About />}></Route>
+          <Route path='/notfound' element={<NotFound />}></Route>
+          <Route path='/*' element={<NotFound />}></Route>
+        </Routes>
+      </main>
+      <Footer />
     </div>
+    </Router>
   );
 }
 
